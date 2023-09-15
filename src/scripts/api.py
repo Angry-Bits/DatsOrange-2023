8bffe637c29e933eab56509658b8bb6293e4deeb
Igor Gakhov <gakhov_igor@rambler.ru>
2023-09-15 18:16:33 +0300

    Update endpoints

--- src/scripts/api.py
+++ src/scripts/api.py
@@ -2,68 +2,12 @@ import httpx
 
 from src.settings import TEAM_TOKEN
 
-def get_sell_stock():
-    url = "https://datsorange.devteam.games/sellStock"
-    header = {"token": TEAM_TOKEN}
 
-    r = httpx.get(url, headers=header)
-    return r.json()
+# place-bid-controller
 
-
-def get_symbols():
-    url = "https://datsorange.devteam.games/getSymbols"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-    return r.json()
-
-
-def buy_Stock():
-    url = "https://datsorange.devteam.games/buyStock"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-    return r.json()
-
-
-# news-controller
-
-def latest_news():
-    url = "https://datsorange.devteam.games/news/LatestNews"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-    return r.json()
-
-
-def latest_news_5_minutes():
-    url = "https://datsorange.devteam.games/news/LatestNews5Minutes"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-    return r.json()
-
-
-def latest_news_1_minutes():
-    url = "https://datsorange.devteam.games/news/LatestNews1Minute"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-    news = r.json()
-    # print(r.json())
-    return news
-
-# info-controller
-
-def info():
-    url = "https://datsorange.devteam.games/info"
-    header = {"token": TEAM_TOKEN}
-
-    r = httpx.get(url, headers=header)
-
-def cancel_bid(bid_id: int):
+def remove_bid(bid_id: int):
     """
-    Отменить заявку по заданному ID
+    Отменяет заявку.
     """
     url = "https://datsorange.devteam.games/RemoveBid"
     header = {"token": TEAM_TOKEN}
@@ -73,9 +17,21 @@ def cancel_bid(bid_id: int):
     return r.json()
 
 
+def limit_price_sell(bid_id: int):
+    """
+    Размещает заявку на продажу по цене не менее установленной.
+    """
+    url = "https://datsorange.devteam.games/LimitPriceSell"
+    header = {"token": TEAM_TOKEN}
+    payload = {"bidId": bid_id}
+
+    r = httpx.post(url, headers=header, json=payload)
+    return r.json()
+
+
 def place_buy_order(symbol_id: int, price: int, quantity: int):
     """
-    Размещает заявку на покупку по цене не более установленной
+    Размещает заявку на покупку по цене не более установленной.
     """
     url = "https://datsorange.devteam.games/LimitPriceBuy"
     header = {"token": TEAM_TOKEN}
@@ -89,7 +45,7 @@ def place_buy_order(symbol_id: int, price: int, quantity: int):
     return r.json()
 
 
-def sell_at_best_price(symbol_id: int, quantity: int):
+def best_price_sell(symbol_id: int, quantity: int):
     """
     Разместить заявку на продажу актива по лучшей доступной цене.
     """
@@ -104,7 +60,7 @@ def sell_at_best_price(symbol_id: int, quantity: int):
     return r.json()
 
 
-def buy_at_best_price(symbol_id: int, quantity: int):
+def best_price_buy(symbol_id: int, quantity: int):
     """
     Разместить заявку на покупку актива по лучшей доступной цене.
     """
@@ -116,4 +72,86 @@ def buy_at_best_price(symbol_id: int, quantity: int):
     }
 
     r = httpx.post(url, headers=headers, json=payload)
+    return r.json()
+
+
+# stock-info
+
+def sell_stock():
+    """
+    Заявки на продажу.
+    """
+    url = "https://datsorange.devteam.games/sellStock"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+def get_symbols():
+    """
+    Список всех активов.
+    """
+    url = "https://datsorange.devteam.games/getSymbols"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def buy_stock():
+    """
+    Заявки на покупку.
+    """
+    url = "https://datsorange.devteam.games/buyStock"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+# news-controller
+
+def latest_news():
+    """
+    Возвращает последнюю новость.
+    """
+    url = "https://datsorange.devteam.games/news/LatestNews"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def latest_news_5_minutes():
+    """
+    Возвращает новости за последние 5 минут.
+    """
+    url = "https://datsorange.devteam.games/news/LatestNews5Minutes"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def latest_news_1_minutes():
+    """
+    Возвращает новости за последнюю минуту.
+    """
+    url = "https://datsorange.devteam.games/news/LatestNews1Minute"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+# info-controller
+
+def info():
+    """
+    Информация о счете.
+    """
+    url = "https://datsorange.devteam.games/info"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
     return r.json()
