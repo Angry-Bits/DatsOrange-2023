b68b40605d4e4c9528dd91a6887239b8805e19fa
Igor Gakhov <gakhov_igor@rambler.ru>
2023-09-15 17:50:23 +0300

    Add endpoints

--- src/scripts/api.py
+++ src/scripts/api.py
@@ -1,24 +1,65 @@
 import httpx
 
 from src.settings import TEAM_TOKEN
-def get_sell_stock():
-    """
-    
-    """
+
+
+# stock-info
+
+def sell_stock():
     url = "https://datsorange.devteam.games/sellStock"
     header = {"token": TEAM_TOKEN}
 
     r = httpx.get(url, headers=header)
-    print(r.json())
+    return r.json()
 
 
+def get_symbols():
+    url = "https://datsorange.devteam.games/getSymbols"
+    header = {"token": TEAM_TOKEN}
 
-def get_news():
-    """
-    Получить последние новости за 1 минуту
-    """
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def buy_Stock():
+    url = "https://datsorange.devteam.games/buyStock"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+# news-controller
+
+def latest_news():
+    url = "https://datsorange.devteam.games/news/LatestNews"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def latest_news_5_minutes():
+    url = "https://datsorange.devteam.games/news/LatestNews5Minutes"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
+
+
+def latest_news_1_minutes():
     url = "https://datsorange.devteam.games/news/LatestNews1Minute"
     header = {"token": TEAM_TOKEN}
 
     r = httpx.get(url, headers=header)
-    print(r.json())
+    return r.json()
+
+
+# info-controller
+
+def info():
+    url = "https://datsorange.devteam.games/info"
+    header = {"token": TEAM_TOKEN}
+
+    r = httpx.get(url, headers=header)
+    return r.json()
